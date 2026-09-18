d250787befe20658827c23128c4e59afa148fcec
faangi96 <angirillo96@gmail.com>
2026-09-18 18:15:49 +0200

    Delete sql directory

--- sql/Nachrichten.sql
+++
@@ -1,9 +0,0 @@
--- Datenbank erstellen
-CREATE DATABASE IF NOT EXISTS Nachrichten;
-
-CREATE TABLE nachrichten(
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    email VARCHAR(30)  NOT NULL,
-    betreff VARCHAR (100) NOT NULL,
-    nachricht VARCHAR(500) NOT NULL
-);
